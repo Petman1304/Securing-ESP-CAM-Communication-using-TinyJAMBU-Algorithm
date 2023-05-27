ace420f9259dc2741261b51b3566786b8e66c0c7
unknown <petrusnmanurung@gmail.com>
2023-05-27 20:22:57 +0700

    modifikasi komunikasi esp32 menjadi komunikasi TCP

--- ESP/ESP_cam/ESP_cam.ino
+++ ESP/ESP_cam/ESP_cam.ino
@@ -1,3 +1,6 @@
+#include <ArduinoJson.h>
+#include <ArduinoJson.hpp>
+#include <WiFiClient.h>
 #include <Arduino.h>
 #include <string.h>
 #include "tinyjambu_crypto_aead.h"
@@ -30,12 +33,19 @@
 #define NONCEBYTES 12
 #define TAGBYTES 8
 
-WiFiClient espClient;
-PubSubClient client(espClient);
-unsigned long lastMsg = 0;
-#define MSG_BUFFER_SIZE	(50)
-char msg[MSG_BUFFER_SIZE];
-int value = 0;
+// WiFiClient espClient;
+// PubSubClient client(espClient);
+// unsigned long lastMsg = 0;
+// #define MSG_BUFFER_SIZE	(50)
+// char msg[MSG_BUFFER_SIZE];
+// int value = 0;
+
+
+const char* host = "192.168.246.86"; // IP address of the receiver ESP32
+const uint16_t port = 8888; // Port number to send data
+
+//AsyncWebServer server(80);
+WiFiClient client;
 
 #define FILE_PHOTO "/photo.jpg"
 
@@ -60,52 +70,54 @@ int value = 0;
 // Replace with your network credentials
 const char* ssid = "Edulag";
 const char* password = "98765432";
-const char* mqtt_server = "broker.mqtt-dashboard.com";
+//const char* mqtt_server = "broker.mqtt-dashboard.com";
 
-unsigned char key[KEY_SIZE] = { 0 };      // use a random key instead of all zeros
-unsigned char nonce[NONCEBYTES] = { 0 };  // use a random nonce instead of all zeros
+unsigned char key[KEY_SIZE] = "pmckelompok5123";      // use a random key instead of all zeros
+unsigned char nonce[NONCEBYTES] = "pmckelompk5";  // use a random nonce instead of all zeros
 unsigned char ad[] = { 0 };               // no associated data
 unsigned long adlen = 0;
 
-void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
-  }
-  Serial.println();
+//const int MAX_CHUNK_SIZE = 248; // Chunk size for publishing
 
-  Serial.println("Payload length: " + String(length)); // Menampilkan panjang payload
-  char receivedPayload[length + 1];
-  memcpy(receivedPayload, payload, length);
-  receivedPayload[length] = '\0';
+// void callback(char* topic, byte* payload, unsigned int length) {
+//   // Serial.print("Message arrived [");
+//   // Serial.print(topic);
+//   // Serial.print("] ");
+//   // for (int i = 0; i < length; i++) {
+//   //   Serial.print((char)payload[i]);
+//   // }
+//   // Serial.println();
 
-}
+//   // Serial.println("Payload length: " + String(length)); // Menampilkan panjang payload
+//   // char receivedPayload[length + 1];
+//   // memcpy(receivedPayload, payload, length);
+//   // receivedPayload[length] = '\0';
 
-void reconnect() {
-  // Loop until we're reconnected
-  while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    // Create a random client ID
-    String clientId = "ESP8266Client-";
-    clientId += String(random(0xffff), HEX);
-    // Attempt to connect
-    if (client.connect(clientId.c_str())) {
-      Serial.println("connected");
-      // Once connected, publish an announcement...
-      client.publish("peemce", "hi i'm steve");
-      // ... and resubscribe
-      client.subscribe("peemce");
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
-    }
-  }
-}
+// }
+
+// void reconnect() {
+//   // Loop until we're reconnected
+//   while (!client.connected()) {
+//     Serial.print("Attempting MQTT connection...");
+//     // Create a random client ID
+//     String clientId = "ESP8266Client-";
+//     clientId += String(random(0xffff), HEX);
+//     // Attempt to connect
+//     if (client.connect(clientId.c_str())) {
+//       Serial.println("connected");
+//       // Once connected, publish an announcement...
+//       client.publish("PMC_KEL5", "hi i'm steve");
+//       // ... and resubscribe
+//       client.subscribe("PMC_KEL5");
+//     } else {
+//       Serial.print("failed, rc=");
+//       Serial.print(client.state());
+//       Serial.println(" try again in 5 seconds");
+//       // Wait 5 seconds before retrying
+//       delay(5000);
+//     }
+//   }
+// }
 
 // Check if photo capture was successful
 bool checkPhoto(fs::FS& fs) {
@@ -199,7 +211,7 @@ void mystrlcpy(char* dst, const char* src, size_t size) {
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  Serial.begin(115200);
 
   // Connect to Wi-Fi
   WiFi.begin(ssid, password);
@@ -208,8 +220,8 @@ void setup() {
     Serial.println("Connecting to WiFi...");
   }
   
-  client.setServer(mqtt_server, 1883);
-  client.setCallback(callback);
+  // client.setServer(mqtt_server, 1883);
+  // client.setCallback(callback);
 
   if (!SPIFFS.begin(true)) {
     Serial.println("An Error has occurred while mounting SPIFFS");
@@ -263,15 +275,19 @@ void setup() {
     Serial.printf("Camera init success!");
   }
 
-  client.setServer(mqtt_server, 1883);
-  client.setCallback(callback);
+  
+
+
+  // client.setServer(mqtt_server, 1883);
+  // client.setCallback(callback);
 }
 
 void loop() {
-  if (!client.connected()) {
-    reconnect();
-  }
-  client.loop();
+  // if (!client.connected()) {
+  //   reconnect();
+  // }
+  // client.loop();
+
 
   capturePhotoSaveSpiffs();
   unsigned char* plaintext = read_image();
@@ -302,39 +318,99 @@ void loop() {
   free(plaintext);
 
   Serial.printf("Encrypt success\n");
-  Serial.printf("%d", ciphertext_len);
-  Serial.printf("%s\n", ciphertext);
+  Serial.printf("%d\n", plaintext_len);
+  Serial.printf("%d\n", ciphertext_len);
+  Serial.printf("%s", ciphertext);
+  
+  //Serial.printf("%s\n", ciphertext);
 
   //publish ke mqtt
-  if (client.publish("peemce", (const char*)ciphertext, ciphertext_len)) {
-      Serial.println("Encrypted message published");
-    } else {
-      Serial.println("Failed to publish encrypted message");
+  //client.publish("topic/PMC_KEL5", "Publishing...");
+  // unsigned char stringwannabe[] = "hohaholihgilahighighsrgotvoaetoajtanoyvahsgshgshgiheOIGHoIEHGOIHGIehbsGDKUgsdkglisBG:szighzsihgoisahegiszhgizshgizshguszhguszhgusehguszeifeoshishihsihgihshgohsoghoshgihghshigohrshgirshgsiigrsuhuhsuhuighursgushurhushhgoirshoighoshspjehubwjsbghjfsbjriugrbgsigsgnoisrggusezluiguislgsugsouieha";
+  // if (client.publish("PMC_KEL5", reinterpret_cast<char*>(stringwannabe))) {
+  //     Serial.println("Encrypted message published");
+  //   } else {
+  //     Serial.println("Failed to publish encrypted message");
+  //   }
+
+  if (!client.connected()) {
+    Serial.println("Connecting to server...");
+    if (client.connect(host, port)) {
+      Serial.println("Connected to server");
+      
+      // Create a byte array with dynamic memory
+      // int arraySize = 10;
+      // unsigned char* data = new unsigned char[arraySize];
+      
+      // Populate the array with data
+      // for (int i = 0; i < arraySize; i++) {
+      //   data[i] = i;
+      // }
+      
+      // Send the array size
+      client.write((const uint8_t*)&ciphertext_len, sizeof(ciphertext_len));
+      
+      // Send the array data
+      client.write(ciphertext, ciphertext_len);
+      
+      
     }
+  }
+  free(ciphertext);
+  delay(10000); 
+  
+  // Close the TCP connection
   
 
-  Serial.println("Debug decrypt-1");
-  unsigned char* decrypt = (unsigned char*) malloc (ciphertext_len);
-  //new unsigned char[ciphertext_len];
-  Serial.println("Debug decrypt0");
-  int inv = crypto_aead_decrypt(decrypt, &plaintext_len, NULL, ciphertext, ciphertext_len, ad, adlen, nonce, key);
-  Serial.println("Debug decrypt1");
+    // client.publish_P("PMC_KEL5", &ciphertext[startIdx], endIdx - startIdx, false);
+    // delay(10); // Delay between publishing chunks
+    // }
 
-  if (inv != 0) {
-    Serial.printf("Decrypt failed \n");
-    Serial.println(inv);
-    free(ciphertext);
-    free(decrypt);
-    return;
-  }
+  
+
+  
+    
+  // if (client.publish("PMC_KEL5", jsonString)) {
+  //     Serial.println("Encrypted message published");
+  //   } else {
+  //     Serial.println("Failed to publish encrypted message");
+  //   }
+  
+
+  // Serial.println("Debug decrypt-1");
+  // unsigned char* decrypt = (unsigned char*) malloc (ciphertext_len);
+  // //new unsigned char[ciphertext_len];
+  // Serial.println("Debug decrypt0");
+  // int inv = crypto_aead_decrypt(decrypt, &plaintext_len, NULL, ciphertext, ciphertext_len, ad, adlen, nonce, key);
+  // Serial.println("Debug decrypt1");
+
+  // if (inv != 0) {
+  //   Serial.printf("Decrypt failed \n");
+  //   Serial.println(inv);
+  //   free(ciphertext);
+  //   free(decrypt);
+  //   return;
+  // }
   //delete[] ciphertext;
-  free(ciphertext);
-  Serial.printf("Decrypt success\n");
-  Serial.printf("%s", decrypt);
+  
+  //Serial.printf("Decrypt success\n");
+  //Serial.printf("%s", decrypt);
   //Serial.printf("\n%d", sizeof(decrypt));
-  free(decrypt);
+  //free(decrypt);
   // put your main code here, to run repeatedly:
-  delay(1);  // this speeds up the simulation
+   // this speeds up the simulation
+}
+
+String createMessageFrame(int chunkIndex, int totalChunks, const unsigned char* data, int ciphertext_len) {
+  // Create a message frame with metadata
+  String frame = String(chunkIndex) + ":" + String(totalChunks) + ":";
+
+  // Append the data to the frame
+  for (int i = 0; i < ciphertext_len; i++) {
+    frame += char(data[i]);
+  }
+
+  return frame;
 }
 
 
--- ESP/ESP_sub/ESP_sub/ESP_sub.ino
+++ ESP/ESP_sub/ESP_sub/ESP_sub.ino
@@ -19,6 +19,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <Wire.h>
+#include <WiFiClient.h>
 
 #define KEY_SIZE 16
 #define NONCE_SIZE 16
@@ -30,81 +31,128 @@
 #define NONCEBYTES 12
 #define TAGBYTES 8
 
-WiFiClient espClient;
-PubSubClient client(espClient);
-unsigned long lastMsg = 0;
-#define MSG_BUFFER_SIZE	(50)
-char msg[MSG_BUFFER_SIZE];
-int value = 0;
+//WiFiClient espClient;
+//PubSubClient client(espClient);
+//unsigned long lastMsg = 0;
+//#define MSG_BUFFER_SIZE	(50)
+//char msg[MSG_BUFFER_SIZE];
+//int value = 0;
 
 const char* ssid = "Edulag";
 const char* password = "98765432";
-const char* mqtt_server = "broker.mqtt-dashboard.com";
+//const char* mqtt_server = "broker.mqtt-dashboard.com";
 
-unsigned char key[KEY_SIZE] = { 0 };      // use a random key instead of all zeros
-unsigned char nonce[NONCEBYTES] = { 0 };  // use a random nonce instead of all zeros
+// Port to listen for incoming connections
+const int serverPort = 8888;
+
+WiFiServer server(serverPort);
+WiFiClient client;
+
+unsigned char key[KEY_SIZE] = "pmckelompok5123";      // use a random key instead of all zeros
+unsigned char nonce[NONCEBYTES] = "pmckelompk5";  // use a random nonce instead of all zeros
 unsigned char ad[] = { 0 };               // no associated data
 unsigned long adlen = 0;
 
-void callback(char* topic, byte* payload, unsigned int length) {
-  unsigned long long ciphertext_len = length;
-  unsigned char* ciphertext = (unsigned char*) malloc (ciphertext_len);
+//const int MAX_DATA_SIZE = 15360;
+//const int MAX_CHUNK_SIZE = 256;// Chunk size for publishing
 
-  unsigned long long plaintext_len = 0;
+//unsigned char receivedData[MAX_DATA_SIZE];  // Variable to hold the complete data
+//int receivedSize = 0;  // Size of the received data (incremented as chunks arrive)
 
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
-  
-  // Mengisi array ciphertext dengan data dari payload
-  for (int i = 0; i < length; i += 2) {
-    char hex[3] = {payload[i], payload[i+1], '\0'};
-    unsigned char value = strtol(hex, NULL, 16);
-    ciphertext[i/2] = value;
-  }
+
+
+
+//void callback(char* topic, byte* payload, unsigned int length) {
+//  // Handle incoming MQTT messages
+//  if (strcmp(topic, "topic/esp32/header") == 0) {
+//    // Parse the header message to get total length and current position
+//    String header = String((char*)payload, length);
+//    int commaIdx = header.indexOf(',');
+//    int totalLength = header.substring(0, commaIdx).toInt();
+//    int currentPosition = header.substring(commaIdx + 1).toInt();
+//
+//    // If it's the first chunk, reset the received data buffer
+//    if (currentPosition == 0) {
+//      receivedSize = 0;
+//    }
+//
+//    // Check if the received data exceeds the maximum expected size
+//    if (receivedSize + length - header.length() > MAX_DATA_SIZE) {
+//      Serial.println("Data size exceeds the maximum expected size");
+//      return;
+//    }
+//
+//    // Copy the received data to the buffer
+//    memcpy(&receivedData[receivedSize], payload + header.length(), length - header.length());
+//    receivedSize += length - header.length();
+//
+//    // If it's the last chunk, the complete data has been received
+//    if (currentPosition == totalLength - 1) {
+//      Serial.println("Complete data received");
+//      // Use the receivedData buffer containing the complete data
+//      // Perform any necessary processing or handling of the data here
+//      Serial.printf("%s", receivedData);
+//      
+//      unsigned long long plaintext_len = 0;
+//      unsigned char* decrypt = (unsigned char*) malloc (receivedSize);
+//      int inv = crypto_aead_decrypt(decrypt, &plaintext_len, NULL, receivedData, receivedSize, ad, adlen, nonce, key);
+//  
+//       if (inv != 0) {
+//         Serial.printf("Decrypt failed \n");
+//         Serial.println(inv);
+//         //free(ciphertext);
+//         free(decrypt);
+//         return;
+//       }
+//    
+//       Serial.printf("%s", decrypt);
+//    }
+//  }
+//}
+
 
   // Dekripsi ciphertext dan mencetak decrypted_plaintext sebagai string
-  unsigned char* decrypt = (unsigned char*) malloc (ciphertext_len);
-  int inv = crypto_aead_decrypt(decrypt, &plaintext_len, NULL, ciphertext, ciphertext_len, ad, adlen, nonce, key);
+  // unsigned char* decrypt = (unsigned char*) malloc (ciphertext_len);
+  // int inv = crypto_aead_decrypt(decrypt, &plaintext_len, NULL, ciphertext, ciphertext_len, ad, adlen, nonce, key);
   
-  if (inv != 0) {
-    Serial.printf("Decrypt failed \n");
-    Serial.println(inv);
-    free(ciphertext);
-    free(decrypt);
-    return;
-  }
+  // if (inv != 0) {
+  //   Serial.printf("Decrypt failed \n");
+  //   Serial.println(inv);
+  //   free(ciphertext);
+  //   free(decrypt);
+  //   return;
+  // }
 
-  Serial.printf("%s", decrypt);
-}
+  // Serial.printf("%s", decrypt);
+//}
 
-void reconnect() {
-  // Loop until we're reconnected
-  while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    // Create a random client ID
-    String clientId = "ESP8266Client-";
-    clientId += String(random(0xffff), HEX);
-    // Attempt to connect
-    if (client.connect(clientId.c_str())) {
-      Serial.println("connected");
-      // Once connected, publish an announcement...
-      client.publish("peemce", "hi steve");
-      // ... and resubscribe
-      client.subscribe("peemce");
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
-    }
-  }
-}
+//void reconnect() {
+//  // Loop until we're reconnected
+//  while (!client.connected()) {
+//    Serial.print("Attempting MQTT connection...");
+//    // Create a random client ID
+//    String clientId = "ESP8266Client-";
+//    clientId += String(random(0xffff), HEX);
+//    // Attempt to connect
+//    if (client.connect(clientId.c_str())) {
+//      Serial.println("connected");
+////      // Once connected, publish an announcement...
+////      client.publish("peemce", "hi steve");
+////      // ... and resubscribe
+////      client.subscribe("peemce");
+//    } else {
+//      Serial.print("failed, rc=");
+//      Serial.print(client.state());
+//      Serial.println(" try again in 5 seconds");
+//      // Wait 5 seconds before retrying
+//      delay(5000);
+//    }
+//  }
+//}
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  Serial.begin(115200);
 
   // Connect to Wi-Fi
   WiFi.begin(ssid, password);
@@ -113,18 +161,110 @@ void setup() {
     Serial.println("Connecting to WiFi...");
   }
   
-  client.setServer(mqtt_server, 1883);
-  client.setCallback(callback);
+//  client.setServer(mqtt_server, 1883);
+//  client.setCallback(callback);
 
+//  client.subscribe("topic/PMC_KEL5/header");
+//  client.subscribe("topic/PMC_KEL5/data");
 
+ // Start the TCP server
+  //WiFiServer server(serverPort);
+  server.begin();
+  
+  Serial.println("Server started");
+  Serial.print("IP Addr :");
+  Serial.println(WiFi.localIP());
+  
+  
+
+  // Dekripsi ciphertext dan mencetak decrypted_plaintext sebagai string
+  // unsigned char* decrypt = (unsigned char*) malloc (ciphertext_len);
+  // int inv = crypto_aead_decrypt(decrypt, &plaintext_len, NULL, ciphertext, ciphertext_len, ad, adlen, nonce, key);
+  
+  // if (inv != 0) {
+  //   Serial.printf("Decrypt failed \n");
+  //   Serial.println(inv);
+  //   free(ciphertext);
+  //   free(decrypt);
+  //   return;
+  // }
+
+  // Serial.printf("%s", decrypt);
+  
+
+  //client.stop();
+
+  
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-    if (!client.connected()) {
-    reconnect();
-  }
-  client.loop();
+if (!client.connected()) {
+    client = server.available();
+    if (client) {
+      Serial.println("Client connected");
+      
+      // Receive the array size
+      unsigned long long ciphertext_len;
+      Serial.println("debug1");
+      if (client.available() >= sizeof(ciphertext_len)) {
+        Serial.println("debug2");
+        client.readBytes((char*)&ciphertext_len, sizeof(ciphertext_len));
+        Serial.println("debug3");
+        // Create a buffer to hold the received data
+        unsigned char* ciphertext = new unsigned char[ciphertext_len];
+        Serial.println("debug4");
+        // Receive the array data
+        Serial.println("debug5");
+          client.readBytes((char*)ciphertext, ciphertext_len);
+          Serial.println("debug6");
+          // Process the received array data
+//          for (int i = 0; i < ciphertext_len; i++) {
+//            Serial.print(ciphertext[i]);
+//            Serial.print(" ");
+//          }
+          Serial.printf("%s", ciphertext);
+          Serial.printf("%d\n", ciphertext_len);
+          Serial.println();
 
+          // Dekripsi ciphertext dan mencetak decrypted_plaintext sebagai string
+  unsigned long long plaintext_len = ciphertext_len - TAGBYTES;
+  unsigned char* decrypt = (unsigned char*) malloc (plaintext_len);
+  
+  int inv = crypto_aead_decrypt(decrypt, &plaintext_len, NULL, ciphertext, ciphertext_len, ad, adlen, nonce, key);
+  
+  if (inv != 0) {
+     Serial.printf("Decrypt failed \n");
+     Serial.println(inv);
+     free(ciphertext);
+     free(decrypt);
+     return;
+   }
+   decrypt[plaintext_len]= (unsigned char)'\0';
+
+   Serial.printf("%s", decrypt);
+          
+          // Clean up the dynamically allocated memory
+          delete[] ciphertext;
+        
+//        if (client.available() >= ciphertext_len) {
+//          Serial.println("debug5");
+//          client.readBytes((char*)ciphertext, ciphertext_len);
+//          Serial.println("debug6");
+//          // Process the received array data
+////          for (int i = 0; i < ciphertext_len; i++) {
+////            Serial.print(ciphertext[i]);
+////            Serial.print(" ");
+////          }
+//          Serial.printf("%s", ciphertext);
+//          Serial.println();
+//          
+//          // Clean up the dynamically allocated memory
+//          delete[] ciphertext;
+//        }
+      }
+    }
+  }
+  
+  delay(1);
 }
